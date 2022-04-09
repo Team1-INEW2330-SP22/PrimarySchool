8413d67e541993073ef60406471a3922892b3c6c
Ryan Hicks <rthicks@mymail.tstc.edu>
2022-04-08 20:10:47 -0500

    Updating password recovery system.

--- PrimarySchool/ProgOps.cs
+++ PrimarySchool/ProgOps.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+using System.Configuration;
 
 // Performs various database (DB) related operations for Primary School.
 
@@ -1186,6 +1189,262 @@ namespace PrimarySchool
             {
                 FormOps.ErrorBox(ex.Message);
             }
+        }
+
+        //Establishing Add Command Objects - rth040322
+        private static SqlCommand _sqlUsersCommand;                                  //Cmd Object for Users Table
+
+        //Establishing Data Adapter - rth040322
+        private static SqlDataAdapter _daUsers = new SqlDataAdapter();               //SqlDataAdapter Object for Users Table
+
+        //Establishing Data Tables - rth040322
+        private static DataTable _dtUsersTable = new DataTable();                    //DataTable Object for Users Table
+
+        public static void UserLoginObjectDisposal()
+        {
+            //Dispose of UserLogin command adapter and objects
+            _sqlUsersCommand.Dispose();
+            _daUsers.Dispose();
+            _dtUsersTable.Dispose();
+        }
+
+        public static int UserLogin_DBCmd(TextBox tbxUserName, TextBox tbxUserPassword)
+        {
+            //Set Objects to null
+            _sqlUsersCommand = null;
+            _daUsers = null;
+            _dtUsersTable = null;
+
+            //reset data adapter and datatable to new
+            _daUsers = new SqlDataAdapter();
+            _dtUsersTable = new DataTable();
+
+            //Employees Table SQL Statement
+            string sqlUsers = "SELECT * FROM group1fa212330.Users WHERE User_LoginName = '" + tbxUserName.Text + "' AND User_LoginPwd = '" + tbxUserPassword.Text + "';";
+
+            try
+            {
+                //Establish the Command Object
+                _sqlUsersCommand = new SqlCommand(sqlUsers, _cntPrimarySchoolDatabase);
+
+                //Establish Data Adapter
+                _daUsers.SelectCommand = _sqlUsersCommand;
+
+                //Establish Data Table
+                _daUsers.Fill(_dtUsersTable);
+
+            }
+            catch (Exception ex)
+            {
+                //Message stating connection was successfull
+                MessageBox.Show(ex.Message, "Error in Processing SQL", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            if (_dtUsersTable.Rows.Count > 0)
+            {
+                MessageBox.Show("Welcome to the PrimarySchool application.", "Login Successful");
+                return 1;
+            }
+            else
+            {
+                MessageBox.Show("The username or password you have entered is invalid.", "Invalid Login");
+                return 0;
+            }
+
+        }
+
+        public static int CheckEmail(TextBox tbxEmail)
+        {
+            //Set Objects to null
+            _sqlUsersCommand = null;
+            _daUsers = null;
+            _dtUsersTable = null;
+
+            //reset data adapter and datatable to new
+            _daUsers = new SqlDataAdapter();
+            _dtUsersTable = new DataTable();
+
+            //Employees Table SQL Statement
+            string sqlUser = "SELECT * FROM group1fa212330.Users WHERE User_Email = '" + tbxEmail.Text + "'";
+
+            try
+            {
+                //Establish the Command Object
+                _sqlUsersCommand = new SqlCommand(sqlUser, _cntPrimarySchoolDatabase);
+
+                //Establish Data Adapter
+                _daUsers.SelectCommand = _sqlUsersCommand;
+
+                //Establish Data Table
+                _daUsers.Fill(_dtUsersTable);
+
+            }
+            catch (Exception ex)
+            {
+                //Message stating connection was successfull
+                MessageBox.Show(ex.Message, "Error in Processing SQL", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            if (_dtUsersTable.Rows.Count > 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public static void UpdateTableWithToken(TextBox tbxEmail, string token)
+        {
+            //Set Objects to null
+            _sqlUsersCommand = null;
+            _daUsers = null;
+            _dtUsersTable = null;
+
+            //reset data adapter and datatable to new
+            _daUsers = new SqlDataAdapter();
+            _dtUsersTable = new DataTable();
+
+            //Employees Table SQL Statement
+            string sqlUser = "UPDATE group1fa212330.Users SET User_Email_Token = @token WHERE User_Email = @email";
+
+            try
+            {
+                //Establish the Command Object
+                _sqlUsersCommand = new SqlCommand(sqlUser, _cntPrimarySchoolDatabase);
+
+                _sqlUsersCommand.Parameters.AddWithValue("@token", token);
+                _sqlUsersCommand.Parameters.AddWithValue("@email", tbxEmail.Text);
+
+                _sqlUsersCommand.ExecuteNonQuery();
+                _sqlUsersCommand.Parameters.Clear();
+            }
+            catch (Exception ex)
+            {
+                FormOps.ErrorBox(ex.Message);
+            }
+
+            //Old Code rth0408
+            //using(_cntPrimarySchoolDatabase)
+            //using (_sqlAddTokenCommand = _cntPrimarySchoolDatabase.CreateCommand())
+            //{
+            //    _sqlAddTokenCommand.Parameters.AddWithValue("@token", token);
+            //    _sqlAddTokenCommand.Parameters.AddWithValue("@email", tbxEmail.Text);
+
+            //    _sqlAddTokenCommand.CommandText = "UPDATE group1fa212330.Users SET User_Email_Token = @token WHERE User_Email = @email";
+
+            //    _sqlAddTokenCommand.ExecuteNonQuery();
+            //}
+        }
+
+        public static void emailResetPwd(string email, string token)
+        {
+            using (MailMessage message = new MailMessage(ConfigurationManager.AppSettings["FromEmail"], email))
+            {
+                message.Subject = "Forgotten Password";
+                message.Body = "You reset token is: " + token;
+                message.IsBodyHtml = false;
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = ConfigurationManager.AppSettings["Host"];
+                smtp.EnableSsl = true;
+                NetworkCredential NetworkCred = new NetworkCredential(ConfigurationManager.AppSettings["Username"], ConfigurationManager.AppSettings["Password"]);
+                smtp.UseDefaultCredentials = true;
+                smtp.Credentials = NetworkCred;
+                smtp.Port = int.Parse(ConfigurationManager.AppSettings["Port"]);
+                smtp.Send(message);
+            }
+        }
+
+        public static int TokenChallange(TextBox tbxEmail, TextBox tbxToken)
+        {
+            //Set Command Object to null
+            _sqlUsersCommand = null;
+            _daUsers = null;
+            _dtUsersTable = null;
+
+            //reset data adapter and datatable to new
+            _daUsers = new SqlDataAdapter();
+            _dtUsersTable = new DataTable();
+
+            //Employees Table SQL Statement
+            string sqlToken = "SELECT * FROM group1fa212330.Users WHERE User_Email = '" + tbxEmail.Text + "' AND User_Email_Token = '" + tbxToken.Text + "';";
+
+            try
+            {
+                //Establish the Command Object
+                _sqlUsersCommand = new SqlCommand(sqlToken, _cntPrimarySchoolDatabase);
+
+                //Establish Data Adapter
+                _daUsers.SelectCommand = _sqlUsersCommand;
+
+                //Establish Data Table
+                _daUsers.Fill(_dtUsersTable);
+
+            }
+            catch (Exception ex)
+            {
+                //Message stating connection was successfull
+                MessageBox.Show(ex.Message, "Error in Processing SQL", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            if (_dtUsersTable.Rows.Count > 0)
+            {
+                return 1;
+            }
+            else
+            {
+                MessageBox.Show("Token Challange Failed", "Invalid Login");
+                return 0;
+            }
+
+        }
+
+        public static void UpdateNewPass(string email, string token, TextBox tbxPassword)
+        {
+            //Set Objects to null
+            _sqlUsersCommand = null;
+            _daUsers = null;
+            _dtUsersTable = null;
+
+            //reset data adapter and datatable to new
+            _daUsers = new SqlDataAdapter();
+            _dtUsersTable = new DataTable();
+
+            //Employees Table SQL Statement
+            string sqlUser = "UPDATE group1fa212330.Users SET User_LoginPwd = @newPass, User_Email_Token = NULL WHERE User_Email = @email AND User_Email_Token = @token";
+
+            try
+            {
+                //Establish the Command Object
+                _sqlUsersCommand = new SqlCommand(sqlUser, _cntPrimarySchoolDatabase);
+
+                _sqlUsersCommand.Parameters.AddWithValue("@token", token);
+                _sqlUsersCommand.Parameters.AddWithValue("@email", email);
+                _sqlUsersCommand.Parameters.AddWithValue("@newPass", tbxPassword.Text);
+
+                _sqlUsersCommand.ExecuteNonQuery();
+                _sqlUsersCommand.Parameters.Clear();
+            }
+            catch (Exception ex)
+            {
+                FormOps.ErrorBox(ex.Message);
+            }
+
+
+
+            //Old Code
+            //using (_cntPrimarySchoolDatabase)
+            //using (_sqlUpdatePassCommand = _cntPrimarySchoolDatabase.CreateCommand())
+            //{
+            //    _sqlUpdatePassCommand.Parameters.AddWithValue("@token", token);
+            //    _sqlUpdatePassCommand.Parameters.AddWithValue("@email", email);
+            //    _sqlUpdatePassCommand.Parameters.AddWithValue("@newPass", tbxPassword.Text);
+
+            //    _sqlUpdatePassCommand.CommandText = "UPDATE group1fa212330.Users SET User_LoginPwd = @newPass, User_Email_Token = NULL WHERE User_Email = @email AND User_Email_Token = @token";
+
+            //    _sqlUpdatePassCommand.ExecuteNonQuery();
+            //}
         }
     }
 }
